214ad05c4f00b936894fdfbb3839fe7c4fe34da5
EduPZz <eduardo.pozzogood@hotmail.com>
2023-11-10 00:12:49 -0300

    Endpoint de relatorios

--- back/main/api/views/relatorios.py
+++ back/main/api/views/relatorios.py
@@ -4,6 +4,30 @@ from base.models import Locacao
 from base.models import Empresa
 from ..serializers.locacao import LocacaoSerializer
 from django.core.exceptions import ObjectDoesNotExist
+import statistics
+
+
+def getFaturamentos(locacoes):
+    faturamentos = []
+    for locacao in locacoes:
+        if locacao.foi_pago:
+            faturamentos.append(locacao.valor)
+    return faturamentos
+
+
+def getDevedoresAndPagantes(locacoes):
+    devedores = 0
+    pagantes = 0
+    for locacao in locacoes:
+        if locacao.foi_pago:
+            pagantes += 1
+        if not (locacao.foi_pago):
+            devedores += 1
+    return devedores, pagantes
+
+
+def porcentagem(total, value):
+    return (value / total) * 100
 
 
 def locacaoExists(empresaId):
@@ -13,13 +37,31 @@ def locacaoExists(empresaId):
 @api_view(['GET'])
 def showRelatorio(request, pk):
     try:
-        empresa = Empresa.objects.get(pk=pk)
         locacoes = locacaoExists(pk)
 
         if locacoes:
-            locacoesSerializer = LocacaoSerializer(locacoes, many=True)
-            return Response(locacoesSerializer.data, status=200)
 
-        return Response({'error': 'Did not found any "locacao" associated with this "empresa" '})
+            faturamentos = getFaturamentos(locacoes)
+            faturamento_data = {
+                'faturamentos': faturamentos,
+                'faturamento_total': sum(faturamentos),
+                'mediana_faturamentos': statistics.median(faturamentos),
+                'media_faturamentos': statistics.mean(faturamentos),
+                'varianca_faturamentos': statistics.variance(faturamentos),
+                'desvio_padrao_faturamentos': statistics.stdev(faturamentos),
+            }
+
+            devedores, pagantes = getDevedoresAndPagantes(locacoes)
+            devedores_data = {
+                'porcentagem_pagantes': porcentagem(len(locacoes), pagantes),
+                'porcentagem_devedores': porcentagem(len(locacoes), devedores),
+            }
+
+            return Response({
+                'faturamento': faturamento_data,
+                'devedores_pagantes': devedores_data
+            }, status=200)
+
+        return Response({'error': 'Did not found any "locacao" associated with this "empresa".'})
     except ObjectDoesNotExist:
         return Response({'error': 'Empresa not found'}, status=404)
